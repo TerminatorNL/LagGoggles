9ad8f873c865a49f0c70541bbf6adaa83eec6325
TerminatorNL <leon.philips12@gmail.com>
2017-12-31 14:01:58 +0100

    Made hybrid jar. Allow profiling in singleplayer worlds Do not require Mixin
    (cherry picked from commit 995ccf3)

--- src/main/java/cf/terminator/laggoggles/Main.java
+++ src/main/java/cf/terminator/laggoggles/Main.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 public class Main {
     public static final String MODID = "LagGoggles";
     public static final String MODID_LOWER = "laggoggles";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0";
     public static Logger LOGGER;
 
     @SidedProxy(
